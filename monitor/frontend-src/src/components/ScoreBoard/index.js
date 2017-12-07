e06ee9903873915ddee4166f6adaec358eb78419
Johan Palmfjord <johan.palmfjord@izettle.com>
2017-12-07 14:11:17 +0100

    Add score cards

--- monitor/frontend-src/src/components/ScoreBoard/index.js
+++ monitor/frontend-src/src/components/ScoreBoard/index.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import classes from './style.scss'
+
 const mapState = state => ({
   white: state.score.white,
   red: state.score.red,
 })
 
 export const ScoreBoardComponent = ({ white, red }) => (
-  <div>{white} - {red}</div>
+  <div className={classes.container}>
+    <div className={classes.cardContainer}>
+      <div className={classes.card}>
+        <div className={classes.border} />
+        {white}
+      </div>
+    </div>
+
+    <div className={classes.cardContainer}>
+      <div className={classes.card}>
+        <div className={classes.border} />
+        {red}
+      </div>
+    </div>
+  </div>
 )
 
 export const ScoreBoard = connect(mapState)(ScoreBoardComponent)
